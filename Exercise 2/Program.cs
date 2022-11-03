ae30e1152d12b5f934243d5abe133c04e10c8173
panjialbir <panjialbir10@gmail.com>
2022-11-03 15:53:28 +0700

    membuat class mergesort

--- Exercise 2/Program.cs
+++ Exercise 2/Program.cs
@@ -1,1 +1,31 @@
-﻿
+﻿using System;
+
+namespace MergeSort
+{
+    class MergeSort
+    { //method mergesort
+      static public void MainMerge(int[] numbers, int left, int mid, int right)
+        {
+            int[] temp = new int[27];
+            int 1, eol, num, pos;
+            eol = (mid - 1);
+            pos = left;
+            numbers = (right - left + 1);
+
+            //perulangan
+            while ((left <= eol) && (mid <= right))
+            {
+                if (numbers[left] <= numbers[mid])
+                    temp[pos++] = numbers[left++];
+                else
+                    temp[pos++] = numbers[mid++];
+            }
+            while (left <= eol)
+                temp[pos++] = numbers[left++];
+            while (mid <= right)
+                temp[pos++] = numbers[mid++];
+            for(i )
+
+        }
+    }
+}
